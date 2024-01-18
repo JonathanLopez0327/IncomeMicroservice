d5a1f169584944443c1c0d0ddd3f1b7c96a19a95
jonathanlopez <toribioj818@gmail.com>
2024-01-18 18:02:37 -0400

    Added api gatway change

--- src/main/java/com/budgetproject/incomeservice/external/client/AccountService.java
+++ src/main/java/com/budgetproject/incomeservice/external/client/AccountService.java
@@ -1,11 +1,14 @@
 package com.budgetproject.incomeservice.external.client;
 
+import com.budgetproject.incomeservice.exception.IncomeServiceCustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "ACCOUNT-SERVICE/account")
 public interface AccountService {
     @PutMapping("/debitAmount/{id}")
@@ -19,4 +22,12 @@ public interface AccountService {
             @PathVariable("id") long accountId,
             @RequestParam long amount
     );
+
+    default void fallback(Exception e) {
+        throw new IncomeServiceCustomException(
+                "Account Service is not available",
+                "UNAVAILABLE",
+                500
+        );
+    }
 }
