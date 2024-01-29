0778a0f1945d33074706b18d5fe72977283bf152
jonathanlopez <toribioj818@gmail.com>
2024-01-28 22:44:37 -0400

    Added Spring Security config

--- src/main/java/com/budgetproject/incomeservice/controller/IncomeCategoryController.java
+++ src/main/java/com/budgetproject/incomeservice/controller/IncomeCategoryController.java
@@ -3,10 +3,12 @@ package com.budgetproject.incomeservice.controller;
 import com.budgetproject.incomeservice.model.CategoryRequest;
 import com.budgetproject.incomeservice.model.CategoryResponse;
 import com.budgetproject.incomeservice.service.IncomeCategoryService;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +20,21 @@ public class IncomeCategoryController {
     @Autowired
     private IncomeCategoryService categoryService;
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @PostMapping
     public ResponseEntity<Long> recordCategory(@RequestBody CategoryRequest categoryRequest) {
         long categoryId = categoryService.recordCategory(categoryRequest);
         return new ResponseEntity<>(categoryId, HttpStatus.CREATED);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer", "B_Auditor"})
     @GetMapping
     public ResponseEntity<List<CategoryResponse>> listCategory() {
         List<CategoryResponse> responses = categoryService.listCategory();
         return new ResponseEntity<>(responses, HttpStatus.OK);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @PutMapping("/{id}")
     public ResponseEntity<Void> modifyCategory(
             @PathVariable("id") long categoryId,
@@ -39,6 +44,7 @@ public class IncomeCategoryController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeCategory(
             @PathVariable("id") long categoryId
--- src/main/java/com/budgetproject/incomeservice/controller/IncomeController.java
+++ src/main/java/com/budgetproject/incomeservice/controller/IncomeController.java
@@ -3,10 +3,12 @@ package com.budgetproject.incomeservice.controller;
 import com.budgetproject.incomeservice.model.IncomeRequest;
 import com.budgetproject.incomeservice.model.IncomeResponse;
 import com.budgetproject.incomeservice.service.IncomeService;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +20,21 @@ public class IncomeController {
     @Autowired
     private IncomeService incomeService;
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @PostMapping
     public ResponseEntity<Long> addIncome(@RequestBody @Valid IncomeRequest incomeRequest) {
         long incomeId = incomeService.recordIncome(incomeRequest);
         return new ResponseEntity<>(incomeId, HttpStatus.CREATED);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer", "B_Auditor"})
     @GetMapping
     public ResponseEntity<List<IncomeResponse>> getAllIncome() {
         List<IncomeResponse> incomeResponse = incomeService.getAllIncome();
         return new ResponseEntity<>(incomeResponse, HttpStatus.OK);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer", "B_Auditor"})
     @GetMapping("/{id}")
     public ResponseEntity<IncomeResponse> getIncomeById(@PathVariable("id") long incomeId) {
         IncomeResponse incomeResponse
@@ -37,6 +42,7 @@ public class IncomeController {
         return new ResponseEntity<>(incomeResponse, HttpStatus.OK);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateIncome(@PathVariable("id") long incomeId,
                                              @RequestBody @Valid IncomeRequest incomeRequest) {
@@ -44,6 +50,7 @@ public class IncomeController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @RolesAllowed({"B_Administrator","B_Regular_Customer"})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteIncome(@PathVariable("id") long incomeId) {
         incomeService.deleteIncome(incomeId);
